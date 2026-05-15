dc5ace1fdb30970a01bc7bec6e422057831b45e3
Edvin CANDON <edvin.candon@proton.ch>
2026-05-15 10:02:11 +0200

    Allow by-passing API pending queue & properly handle `TimeoutErrors`
    [IDTEAM-5385]

--- packages/pass/lib/api/factory.ts
+++ packages/pass/lib/api/factory.ts
@@ -20,6 +20,7 @@ import {
     getApiError,
     getApiErrorMessage,
     getIsOfflineError,
+    getIsTimeoutError,
     getIsUnreachableError,
 } from '@proton/shared/lib/api/helpers/apiErrorHelper';
 import { getClientID } from '@proton/shared/lib/apps/helper';
@@ -86,7 +87,7 @@ export const createApi = ({
         const pending = state.get('pendingCount') + 1;
         state.set('pendingCount', pending);
 
-        if (threshold && pending > threshold) {
+        if (threshold && pending > threshold && !options.prioritize) {
             const trigger = awaiter<void>();
             state.get('queued').push(trigger);
             await trigger;
@@ -148,9 +149,11 @@ export const createApi = ({
                 const silent = getSilenced(options, code);
 
                 const networkError = code === PassErrorCode.SERVICE_NETWORK_ERROR;
+                const timedOut = getIsTimeoutError(e);
+                const offline = getIsOfflineError(e);
                 const missingScope = code === PassErrorCode.MISSING_SCOPE;
                 const restricted = isAccessRestricted(code, options.url);
-                const online = !(getIsOfflineError(e) || networkError);
+                const online = !(offline || timedOut || networkError);
                 const unreachable = getIsUnreachableError(e);
                 const sessionLocked = e.name === 'LockedSession';
                 const sessionInactive = e.name === 'InactiveSession' || code === PassErrorCode.SRP_ERROR;
